7c8ab6ed5b5e5b3a399241ea38e63957ce68ff46
L from RW <hasibul.lenin@gmail.com>
2026-03-09 12:54:03 -0400

    refactoring code

--- scripts/data.js
+++ scripts/data.js
@@ -1,86 +1,24 @@
-const fetchAllIssue = function () {
+const setTabData = function () {
   fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
     .then((response) => response.json())
     .then((json) => {
-      console.log(json.data[0]);
-
       const allTab = document.getElementById("accordion-all");
       allTab.innerHTML = "";
-
 
       json.data.forEach(issue => {
         makeCardView(issue, allTab);
       });
 
-
-
     }
-
-
-
-
 
     )
 };
 
 function makeCardView(data, tab) {
 
-  let labels = "";
-  if (Array.isArray(data.labels)) {
-    data.labels.forEach(element => {
-
-      if (element === "help wanted") {
-
-        labels += `
-          <span class="ml-1 badge badge-xs badge-soft border-warning badge-warning uppercase">
-            <i class="fa-solid fa-life-ring"></i>
-            ${element}
-          </span>
-          `;
-
-      } 
-      else if (element === "bug") {
-          labels += `
-          <span class="ml-1 badge badge-xs badge-soft border-error badge-error uppercase">
-            <i class="fa-solid fa-bug"></i>
-            ${element}
-          </span>
-          `;
-      }  
-      else if (element === "enhancement") {
-          labels += `
-          <span class="ml-1 badge badge-xs badge-soft border-success badge-success uppercase">
-            <i class="fa-solid fa-wand-magic-sparkles"></i>
-            ${element}
-          </span>
-          `;
-      }
-        
-      else if (element === "documentation") {
-          labels += `
-          <span class="ml-1 badge badge-xs badge-soft border-black badge-secondry uppercase">
-            <i class="fa-solid fa-file-lines"></i>
-            ${element}
-          </span>
-          `;
-      }
-      else {
-        labels += `
-          <span class="ml-1 badge badge-xs badge-soft border-primary badge-primary uppercase">
-            <i class="fa-solid fa-life-ring"></i>
-            ${element}
-          </span>
-          `;
-      }
-
-    });
-
-  }
-
-
-  let xxxx = document.createElement("div");
-  let vvvvv = `
-    <div class="card bg-base-100 shadow-sm border-t-4 ${data.status === "open" ? "border-success" : "border-primary" }">
+  const card = document.createElement("div");
+  const cardElement = `
+    <div class="card bg-base-100 shadow-sm border-t-4 ${data.status === "open" ? "border-success" : "border-primary"}">
                 <div class="p-4 space-y-2.5">
                     <div class="flex justify-between items-center">
                         <div>
@@ -94,17 +32,71 @@ function makeCardView(data, tab) {
                         <h4 class="font-semibold text-sm capitalize">${data.title}</h4>
                         <p class="text-xs">${data.description}</p>
                     </div>
-                    <p>` + labels + `</p>
+                    <p>` + makeLableView(data.labels) + `</p>
                   </div>
 
                 <hr class="text-base-content/10">
                 <div class="p-4 text-xs text-base-content/30">
-                    <p>#1 by john_doe</p>
-                    <p>1/15/2024</p>
+                    <p>${data.author} is assigned ${data.assignee.length !== 0 ? `by ${data.assignee}` : ""}</p>
+                    <p><time>${new Date(data.createdAt).toLocaleDateString()}</time></p>
                 </div>
             </div>
     
     `;
-  xxxx.innerHTML = vvvvv;
-  tab.append(xxxx);
+  card.innerHTML = cardElement;
+  tab.append(card);
+}
+
+function makeLableView(data) {
+  let labels = "";
+  for (const element of data) {
+
+    if (element === "help wanted") {
+
+      labels += `
+          <span class="ml-1 badge badge-xs badge-soft border-warning badge-warning uppercase">
+            <i class="fa-solid fa-life-ring"></i>
+            ${element}
+          </span>
+          `;
+
+    }
+    else if (element === "bug") {
+      labels += `
+          <span class="ml-1 badge badge-xs badge-soft border-error badge-error uppercase">
+            <i class="fa-solid fa-bug"></i>
+            ${element}
+          </span>
+          `;
+    }
+    else if (element === "enhancement") {
+      labels += `
+          <span class="ml-1 badge badge-xs badge-soft border-success badge-success uppercase">
+            <i class="fa-solid fa-wand-magic-sparkles"></i>
+            ${element}
+          </span>
+          `;
+    }
+
+    else if (element === "documentation") {
+      labels += `
+          <span class="ml-1 badge badge-xs badge-soft border-black badge-secondry uppercase">
+            <i class="fa-solid fa-file-lines"></i>
+            ${element}
+          </span>
+          `;
+    }
+    else {
+      labels += `
+          <span class="ml-1 badge badge-xs badge-soft border-primary badge-primary uppercase">
+            <i class="fa-solid fa-life-ring"></i>
+            ${element}
+          </span>
+          `;
+    }
+
+  }
+
+  return labels;
+
 }
