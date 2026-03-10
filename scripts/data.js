8e9ad853800d25c11931f5706b63de6942c20b5c
Hasibul Lenin <hasibul.lenin@gmail.com>
2026-03-09 21:14:48 -0400

    modal design and implemented

--- scripts/data.js
+++ scripts/data.js
@@ -44,6 +44,7 @@ function makeCardView(data, tabName) {
 
   const cardDiv = document.createElement("div");
   const cardElement = `
+  <a id="${data.id}" onclick="openModal(this.id)">
     <div class="h-full card bg-base-100 shadow-sm border-t-4 ${data.status === "open" ? "border-success" : "border-primary"}">
                 <div class="p-4 space-y-2.5">
                     <div class="flex justify-between items-center">
@@ -56,7 +57,7 @@ function makeCardView(data, tabName) {
                     </div>
                     <div>
                         <h4 class="font-semibold text-sm capitalize">${data.title}</h4>
-                        <p class="text-xs">${data.description}</p>
+                        <p class="text-xs line-clamp-2">${data.description}</p>
                     </div>
                     <p>` + makeLableView(data.labels) + `</p>
                   </div>
@@ -67,7 +68,7 @@ function makeCardView(data, tabName) {
                     <p><time>${new Date(data.createdAt).toLocaleDateString()}</time></p>
                 </div>
             </div>
-    
+    </a>
     `;
   cardDiv.innerHTML = cardElement;
   tabName.append(cardDiv);
@@ -125,4 +126,50 @@ function makeLableView(data) {
 
   return labels;
 
+}
+
+function openModal(id) {
+
+  fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`)
+    .then(response => response.json())
+    .then(json => {
+
+      const modal = document.getElementById("modal-content");
+      const contentDiv = document.createElement("div");
+
+      contentDiv.innerHTML = `
+      <div class="mb-6">
+            <h3 class="text-2xl font-bold mb-2">${json.data.title}</h3>
+            <div class="flex items-center">
+            <span class="capitalize badge badge-sm ${(json.data.status === "open") ? "badge-success" : "badge-primary"} text-white">${json.data.status}</span>
+            <span class="text-base-content/30 text-xs">&nbsp; • &nbsp; Opened by ${json.data.author} &nbsp; •&nbsp; ${new Date(json.data.createdAt).toLocaleDateString()}</span>
+            </div>
+        </div>
+        <div class="mb-6">`
+            + makeLableView(json.data.labels) +
+        `</div>
+        <div class="mb-6">
+            <p class="text-base-content/30">${json.data.description}</p>
+        </div>
+        <div class="grid grid-cols-2 py-4 px-2 bg-[#F8FAFC] rounded-xl">
+            <div>
+                <p class="text-base-content/30">Assignee:</p>
+                <p class="font-semibold">${json.data.assignee}</p>
+            </div>
+            <div>
+                <p class="text-base-content/30">Priority:</p>
+                <p>
+                <span class="badge badge-xs text-white ${json.data.priority === "high" ? "badge-error" : json.data.priority === "medium" ? "badge-warning" : "badge-neutral"} uppercase">
+                            ${json.data.priority}
+                        </span>
+                </p>
+            </div>
+        </div>
+      `
+      modal.innerHTML = "";
+      modal.append(contentDiv);
+    })
+    
+
+  details_modal.showModal();
 }
